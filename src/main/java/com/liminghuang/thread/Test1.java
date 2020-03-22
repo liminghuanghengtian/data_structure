fd3c1601aa5b2ab99a0ff831fb06500c7bbd1894
liminghuang <1286030504@qq.com>
2020-03-22 20:22:28 +0800

    fixed: 临界操作;

--- src/main/java/com/liminghuang/thread/Test1.java
+++ src/main/java/com/liminghuang/thread/Test1.java
@@ -5,7 +5,7 @@ import java.util.concurrent.locks.ReentrantLock;
 /**
  * ProjectName: example
  * PackageName: com.liminghuang.thread
- * Description:
+ * Description: 共享资源临界操作加锁保证线程安全
  * <p>
  * CreateTime: 2017/7/9 18:24
  * Modifier: Adaministrator
@@ -15,52 +15,61 @@ import java.util.concurrent.locks.ReentrantLock;
  * @author Adaministrator
  */
 public class Test1 {
+    // 公平锁会按开启顺序进行锁分配，此处非公平锁
     private ReentrantLock lock = new ReentrantLock(false);
     private int data = 100;
     
     private void increment() {
-        // lock.lock();
+        lock.lock();
         try {
             data++;
-            System.out.println("自增1，data=" + data);
+            System.out.println(Thread.currentThread() + " 自增1，data=" + data);
         } catch (Exception e) {
         
         } finally {
-            // lock.unlock();
+            lock.unlock();
         }
     }
     
     private void decrement() {
-        // lock.lock();
+        lock.lock();
         try {
             data--;
-            System.out.println("自减1，data=" + data);
+            System.out.println(Thread.currentThread() + " 自减1，data=" + data);
         } catch (Exception e) {
         
         } finally {
-            // lock.unlock();
+            lock.unlock();
         }
     }
     
     public static void main(String[] args) throws InterruptedException {
         final Test1 test1 = new Test1();
-        for (int i = 0; i < 20; i++) {
-            new Thread(new Runnable() {
-                public void run() {
-                    test1.increment();
-                }
-            }).start();
+        int k = 100;
+        int i = 0;
+        int j = 0;
+        while (k > 0) {
+            System.out.println("k = " + k);
+            if (k % 2 == 0) {
+                new Thread(new Runnable() {
+                    public void run() {
+                        test1.increment();
+                    }
+                }, "inThread-" + i).start();
+                i++;
+            } else {
+                new Thread(new Runnable() {
+                    public void run() {
+                        test1.decrement();
+                    }
+                }, "deThread-" + j).start();
+                j++;
+            }
+            
+            k--;
         }
         
-        for (int j = 0; j < 10; j++) {
-            new Thread(new Runnable() {
-                public void run() {
-                    test1.decrement();
-                }
-            }).start();
-        }
-        
-        Thread.sleep(5000);
+        Thread.sleep(3000);
         System.out.println("now data, data=" + test1.data);
     }
 }
