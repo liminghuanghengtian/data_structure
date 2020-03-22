975959121148cc7114bba2f015272f784d5314d6
liminghuang <1286030504@qq.com>
2020-03-22 19:38:00 +0800

    fixed: join练习;

--- src/main/java/com/liminghuang/thread/join/Test.java
+++ src/main/java/com/liminghuang/thread/join/Test.java
@@ -41,8 +41,16 @@ public class Test {
     public static void main(String[] args) {
         Thread previousThread = Thread.currentThread();
         for (int i = 0; i < 10; i++) {
+            System.out.println("i = " + i);
             SerializableThread serializableThread = new SerializableThread(previousThread, i);
             serializableThread.start();
+            // 1. 主线程和线程SerializableThread0互相等待，进入WAITING，不再创建和运行后续的线程
+            // 2. 如果SerializableThread内部try-catch逻辑注释掉，则创建线程并会按序执行
+            try {
+                serializableThread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
             previousThread = serializableThread;
         }
         System.out.println("主线程执行完毕");
