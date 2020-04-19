0bad8b5bee5029882dd3b22872c8928fb44d132f
liminghuang <1286030504@qq.com>
2020-04-19 20:54:41 +0800

    fixed: update;

--- src/main/java/com/liminghuang/sort/QuickSort.java
+++ src/main/java/com/liminghuang/sort/QuickSort.java
@@ -5,7 +5,7 @@ package com.liminghuang.sort;
  * PackageName: com.liminghuang.sort
  * Description: 快速排序
  * 原理：
- * <p></p>
+ * <p>{@link com.liminghuang.search.BinarySearch#quickSort(int[])}</p>
  * <p>
  * CreateTime: 2020/4/19 11:28
  * Modifier: Adaministrator
