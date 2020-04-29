d54918969f27ce09cd0eac0e29109bb07d1970b1
liminghuang <1286030504@qq.com>
2020-04-29 22:05:32 +0800

    fixed：test；

---
+++ src/main/java/com/liminghuang/Test.java
@@ -0,0 +1,31 @@
+package com.liminghuang;
+
+/**
+ * ProjectName: data_structure
+ * PackageName: com.liminghuang
+ * Description: 测试类
+ * <p>
+ * CreateTime: 2020/4/18 15:19
+ * Modifier: Adaministrator
+ * ModifyTime: 2020/4/18 15:19
+ * Comment:
+ *
+ * @author Adaministrator
+ */
+public class Test {
+    public static void main(String[] args) {
+        //
+        // System.out.println(-1 >>> 1);
+        // System.out.println(-1 >> 2);
+        //
+        // System.out.println(Integer.toBinaryString(-1));
+        // System.out.println(Integer.toBinaryString(~-1));
+        // System.out.println(Integer.toBinaryString(~0));
+        // System.out.println(Integer.toBinaryString(~9));
+        //
+        // System.out.println(0 >>> 1);// 无符号移动
+        // System.out.println(0 >> 1);
+        System.out.println(~0);
+        System.out.println(~1);
+    }
+}
